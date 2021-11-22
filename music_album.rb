d1286a3bfc48f5d85e3fc64a5937d54f5bb75774
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-11-22 18:09:34 +0100

    Fix Rubocop Linter Errors

--- music_album.rb
+++ music_album.rb
@@ -2,12 +2,13 @@ require_relative 'item'
 
 class MusicAlbum < Item
   def initialize(publish_date, on_spotify: false, archived: false)
-    super(publish_date)
+    super(publish_date, archived)
     @on_spotify = on_spotify
   end
 
   private
+
   def can_be_archived?
-    super && @on_spotify == true ? true : false
+    super && @on_spotify == true
   end
 end
