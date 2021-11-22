6ccf52dbd5eb78cc81c22f166d3b1e2c61b2aa06
faizi2500 <faizan.zahid2500@gmail.com>
2021-11-23 01:00:13 +0500

    Create tests based on TDD approach to validate every method in the class works fine

---
+++ spec/game_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../game'
+require_relative '../item'
+
+describe 'class Game' do
+  before :each do
+    @g1 = Game.new(2009, 2017)
+  end
+
+  it 'check @g1 is kind of Item class' do
+    expect(@g1).to be_kind_of Item
+  end
+
+  it 'check @g1 publish date ' do
+    expect(@g1.publish_date).to eq 2009
+  end
+
+  it 'check @g1 last played date ' do
+    expect(@g1.last_played_at).to eq 2017
+  end
+
+  it 'check if should be moved to archive ' do
+    expect(@g1.move_to_archive).to be true
+  end
+
+  it 'check if should not moved to archive' do
+    @g2 = Game.new(2009, 2020)
+    expect(@g2.move_to_archive).to be nil
+  end
+end
