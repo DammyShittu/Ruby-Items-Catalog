7e5818fc07f4e5161435ab5ae6a80988b964e4ce
Reshy <reshy@protonmail.com>
2021-11-22 14:25:54 +0100

    Minor setup fix

---
+++ item.rb
@@ -0,0 +1,17 @@
+class Item
+    def initialize(publish_date)
+        @id = Random.rand(1..1000)
+        @publish_date = publish_date
+        @archived = false
+    end
+
+    def move_to_archive
+        can_be_archived? ? @archived = true : nil
+    end
+
+    private
+    def can_be_archived?
+        year = Time.new.year
+        year - @publish_date.year > 10 ? true : false
+    end
+end
