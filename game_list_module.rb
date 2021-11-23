6c82d78b9cc183720da5f06f5c9b4d9e529ae61f
faizi2500 <faizan.zahid2500@gmail.com>
2021-11-24 02:02:10 +0500

    Update display methods to display games and authors

---
+++ game_list_module.rb
@@ -0,0 +1,27 @@
+module DisplayList
+  # rubocop: disable Layout/LineLength
+  def display_games
+    puts 'No Games to display' if @games.empty?
+    @games.each_with_index do |game, i|
+      date_obj = game.publish_date
+      play_date = game.last_played_at
+      game.move_to_archive
+      puts "#{i + 1} - Published on: #{date_obj[:year]}/#{date_obj[:month]}/#{date_obj[:day]} | Last Played: #{play_date[:year]}/#{play_date[:month]}/#{play_date[:day]} | Archived: #{game.archived}"
+    end
+    puts ' -------------------------------- '
+  end
+
+  def display_authors
+    puts 'No Authors to display' if @authors.empty?
+    @authors.each_with_index do |val, i|
+      puts "#{i + 1} - #{val.f_name} #{val.l_name} author of following games"
+      puts 'This author has no games to show' if val.items.empty?
+      val.items.each_with_index do |game, j|
+        published_on = game.publish_date
+        puts "Game #{j + 1} - Published on #{published_on[:year]}/#{published_on[:month]}/#{published_on[:day]}"
+      end
+    end
+    puts ' -------------------------------- '
+  end
+  # rubocop: enable Layout/LineLength
+end
