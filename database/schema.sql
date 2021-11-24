8c6b3c51605a47b5da028c5ff1e3bfd6db2d0dd1
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-11-24 16:13:25 +0100

    Create Four Tables In Schema.sql File

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,23 @@
+CREATE TABLE Label(
+  id                INT GENERATED ALWAYS AS IDENTITY,
+  title             VARCHAR(100),
+  color             VARCHAR(100),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Genre(
+  id                INT GENERATED ALWAYS AS IDENTITY,
+  name              VARCHAR(100),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Author(
+  id                 INT GENERATED ALWAYS AS IDENTITY,
+  first_name         VARCHAR(100),
+  last_name          VARCHAR(100),
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE Book(
   id                INT GENERATED ALWAYS AS IDENTITY,
   label_id          INT,
@@ -9,9 +29,23 @@ CREATE TABLE Book(
   FOREIGN KEY (label_id) REFERENCES Label(id)
 );
 
-CREATE TABLE label(
+CREATE TABLE MusicAlbum(
   id                INT GENERATED ALWAYS AS IDENTITY,
-  title             VARCHAR(100),
-  color             VARCHAR(100),
-  PRIMARY KEY (id)
+  genre_id          INT,
+  publish_date      DATE,
+  archived          BOOLEAN,
+  on_spotify        BOOLEAN,
+  PRIMARY KEY (id),
+  FOREIGN KEY (genre_id) REFERENCES Genre(id)
+);
+
+CREATE TABLE Game(
+  id                INT GENERATED ALWAYS AS IDENTITY,
+  author_id         INT,
+  publish_date      DATE,
+  archived          BOOLEAN,
+  last_played       BOOLEAN,
+  multiplayer       BOOLEAN,
+  PRIMARY KEY (id),
+  FOREIGN KEY (author_id) REFERENCES Author(id)
 );
